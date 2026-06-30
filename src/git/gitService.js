ca19a258cf6db689080d6fb75401ad65fca8bd3e
FabricioTrude <fabriciotrude2006@gmail.com>
2026-06-30 19:47:21 -0300

    Merge remote-tracking branch 'origin/main'

--- src/git/gitService.js
+++ src/git/gitService.js
@@ -1,10 +1,40 @@
 const simpleGit = require("simple-git")
 const {existsSync} = require("fs")
+const {rm} = require("fs/promises")
 
 function workspaceGit(){return simpleGit("workspace")}
 
-async function commitAndPush(message)
-{
+async function prepareWorkspace(){
+    await rm("workspace", {
+        recursive: true,
+        force: true
+    })
+    await cloneWorkspace();
+    const git = workspaceGit();
+    await git.addConfig("user.name", process.env.GIT_AUTHOR_NAME)
+    await git.addConfig("user.email", process.env.GIT_AUTHOR_EMAIL)
+}
+
+async function cloneWorkspace() {
+    console.log("Clonando repositorio...");
+    const git = simpleGit();
+
+    const url = `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}.git`;
+
+    console.log("Clonando:", url);
+
+    await git.clone(url, "workspace");
+
+    const repo = simpleGit("workspace");
+
+    console.log(await repo.raw(["remote", "-v"]));
+
+    console.log(await repo.raw(["branch", "-a"]));
+
+    await repo.checkout("komorebi");
+}
+
+async function commitAndPush(message) {
     await git.add(".")
     const status = await git.status();
     if (status.files.length === 0) return;
@@ -29,50 +59,5 @@ async function pushKomorebi(){
     const git = workspaceGit();
     await git.push("origin", "komorebi", {"--force": null});
 }
-async function prepareWorkspace(){
-    if (!existsSync("workspace")){
-        await cloneWorkspace();
-    } else {
-        await updateWorkspace();
-    }
-    const git = workspaceGit();
-    await git.addConfig("user.name", process.env.GIT_AUTHOR_NAME)
-    await git.addConfig("user.email", process.env.GIT_AUTHOR_EMAIL)
-}
 
-// async function cloneWorkspace(){
-//     const git = workspaceGit();
-//     await git.clone(
-//         `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPOSITORY}.git`,
-//         "workspace"
-//     );
-//     const repo = simpleGit("workspace");
-//     await repo.checkout("komorebi");
-// }
-
-async function cloneWorkspace() {
-    console.log("Clonando repositorio...");
-    const git = simpleGit();
-
-    const url = `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}.git`;
-
-    console.log("Clonando:", url);
-
-    await git.clone(url, "workspace");
-
-    const repo = simpleGit("workspace");
-
-    console.log(await repo.raw(["remote", "-v"]));
-
-    console.log(await repo.raw(["branch", "-a"]));
-
-    await repo.checkout("komorebi");
-}
-
-async function updateWorkspace(){
-    console.log("Actualizando repositorio...");
-    const repo = simpleGit("workspace");
-    await repo.checkout("komorebi")
-    await repo.pull("origin", "komorebi");
-}
 module.exports = {commitAndPush, pushKomorebi, prepareWorkspace}
