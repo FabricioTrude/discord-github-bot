357dbec670d786afe2c391e65877b41f10f7317e
FabricioTrude <fabriciotrude2006@gmail.com>
2026-06-30 23:06:15 -0300

    23:06

---
+++ src/functions.js
@@ -0,0 +1,92 @@
+import {buildThreadMarkdown, generateMarkdown} from "./markdown/generateMarkdown";
+
+const fs = require("fs/promises");
+const {ChannelType} = require("discord.js");
+
+let client
+function setClient(c){client = c}
+
+function getCategoryDir(thread){
+    const name = thread.parent.name.toLowerCase();
+    console.log(name)
+    if (name.includes("core")) return "docs/core"
+    if (name.includes("lab")) return "docs/lab"
+    if (name.includes("loreum")) return "docs/loreum"
+    if (name.includes("arty")) return "docs/arty"
+    if (name.includes("mechanum")) return "docs/mechanum"
+    return "docs/others"
+}
+
+async function syncAllForums(){
+    const guilds = client.guilds.cache
+    for (const guild of guilds.values()){
+        const channels = await guild.channels.fetch();
+        const forums = channels.filter(
+            c => c.type === ChannelType.GuildForum
+        )
+        for (const forum of forums.values()){
+            await syncForum(forum)
+        }
+    }
+}
+
+async function syncForum(forum){
+    const threads =  await forum.threads.fetchActive()
+    const dir = getCategoryDir(forum)
+    const registry = await loadRegistry(dir)
+    for (const thread of threads.threads.values()){
+        const last = thread.lastMessage?.createdTimestamp ?? 0;
+        const saved = registry[thread.id] ?? 0;
+        if (saved !== last){
+            const markdown = await buildThreadMarkdown(thread);
+            await writeThreadFileFromContent(thread, markdown);
+            registry[thread.id] = last;
+        }
+    }
+    await saveRegistry(dir, registry)
+}
+
+async function writeThreadFileFromContent(thread, markdownContent) {
+    const filename = thread.name
+        .toLowerCase()
+        .replaceAll(" ", "-");
+
+    const dir = `docs/${thread.parent.name.toLowerCase()}`;
+    const filePath = `workspace/${dir}/${filename}.md`;
+    await fs.mkdir(`workspace/${dir}`, { recursive: true });
+    await fs.writeFile(filePath, markdownContent);
+    const categoryDir = getCategoryDir(thread);
+    await registry(thread, categoryDir);
+    return filePath;
+}
+
+async function registry(thread, dir){
+    const data = await loadRegistry(dir);
+    data[thread.id] = thread.lastMessage?.createdTimestamp ?? 0;
+    await saveRegistry(dir, data)
+}
+
+async function loadRegistry(dir) {
+    try {
+        return JSON.parse(await fs.readFile(`workspace/${dir}/.registry.json`, "utf-8"));
+    } catch {
+        return {};
+    }
+}
+
+async function saveRegistry(dir, registry) {
+    await fs.writeFile(
+        `workspace/${dir}/.registry.json`,
+        JSON.stringify(registry, null, 2)
+    );
+}
+
+export {
+    getCategoryDir,
+    writeThreadFileFromContent,
+    loadRegistry,
+    saveRegistry,
+    registry,
+    syncAllForums,
+    setClient
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
-const fs = require("fs/promises");
+
+const PORT = process.env.PORT || 3000;
+const http = require("http");
+
+http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("OK");
+}).listen(process.env.PORT, () => {console.log(`Server is listening on port ${PORT}`)});
+
 const {Client, GatewayIntentBits, ChannelType, Partials} = require('discord.js');
 const {commitAndPush, prepareWorkspace} = require("./git/gitService");
-const {generateMarkdown, buildThreadMarkdown} = require("./markdown/generateMarkdown");
+const {buildThreadMarkdown} = require("./markdown/generateMarkdown");
+const {writeThreadFileFromContent, syncAllForums, setClient} = require("./functions");
 
 const client = new Client({
     intents:[
@@ -15,141 +24,59 @@ const client = new Client({
 });
 
 client.once("clientReady", async () => {
+    setClient(client);
     await prepareWorkspace();
     console.log(`${client.user.tag} is online!`);
+    await syncAllForums();
+    console.log("Threads atualizadas!")
 })
 
 client.on("threadCreate", async thread => {
     if (!thread.parent || thread.parent.type !== ChannelType.GuildForum) return;
-    try {
-        const filePath = await createThread(thread);
-        await commitAndPush(`feat: add ${thread.name}.md`);
-        console.log(`CREATED: ${filePath}`);
-    } catch (error) {
-        console.error(error)
-    }
+    scheduleSync(thread);
 })
 
 client.on("messageCreate", async (message) => {
-    if (!message.guild) return;
-    if (!message.channel.isThread()) return;
-    const thread = message.channel;
-    if (thread.parent?.type !== ChannelType.GuildForum) return;
-
-    try {
-        const markdown = await buildThreadMarkdown(thread);
-        const filePath = await writeThreadFileFromContent(thread, markdown);
-
-        await commitAndPush(`docs: update ${thread.name}`);
-
-        console.log("THREAD UPDATED (messageCreate)");
-    } catch (err) {
-        console.error(err);
-    }
-});
+    console.log("mensagem enviada")
+    if (!isForumThread(message)) return;
+    scheduleSync(message.channel);
+})
 
 client.on("messageUpdate", async (_, message) => {
-    if (!message.guild) return;
-    if (!message.channel?.isThread()) return;
+    console.log("mensagem atualizada")
+    if (!isForumThread(message)) return;
+    scheduleSync(message.channel);
+});
 
-    const thread = message.channel;
+function isForumThread(message){
+    if (!message.guild) return false
+    if (!message.channel.isThread()) return false
+    return message.channel.parent?.type === ChannelType.GuildForum;
+}
 
-    if (thread.parent?.type !== ChannelType.GuildForum) return;
-
+async function syncThread(thread){
     try {
         const markdown = await buildThreadMarkdown(thread);
-
         const filePath = await writeThreadFileFromContent(thread, markdown);
 
         await commitAndPush(`docs: update ${thread.name}`);
-
-        console.log("THREAD UPDATED (messageUpdate)");
+        console.log("THREAD UPDATED " + filePath);
     } catch (err) {
         console.error(err);
     }
-});
-
-// client.on("messagereactionAdd", async (reaction, user) => {
-//     if (user.bot) return;
-//     try{
-//         await reaction.fetch();
-//
-//         const emoji = reaction.emoji.name;
-//
-//         if (emoji !== "🚀") return;
-//
-//         const message = reaction.message;
-//
-//         if (!message.channel.isThread()) return;
-//
-//         const thread = message.channel;
-//
-//         if (thread.parent?.type !== ChannelType.GuildForum) return;
-//
-//         console.log("🚀 PUBLISH TRIGGER DETECTED");
-//
-//         await pushKomorebi();
-//
-//         console.log("✅ komorebi pushed");
-//     } catch(e){
-//         console.error(e)
-//     }
-// })
-
-async function createThread(thread) {
-    const starterMessage = await thread.fetchStarterMessage();
-    return await writeThreadFile(thread, starterMessage);
 }
 
+const timers = new Map();
 
-async function writeThreadFile(thread, message) {
-    const markdown = generateMarkdown(thread, message);
-    const filename = thread.name
-        .toLowerCase()
-        .replaceAll(" ", "-");
-    const dir = `docs/${thread.parent.name.toLowerCase()}`;
-    const filePath = `workspace/${dir}/${filename}.md`;
-    await fs.mkdir(`workspace/${dir}`, {recursive: true});
-    await fs.writeFile(filePath, markdown);
-    await registry(thread, filePath);
-    return filePath;
-}
-
-async function writeThreadFileFromContent(thread, markdownContent) {
-    const filename = thread.name
-        .toLowerCase()
-        .replaceAll(" ", "-");
-
-    const dir = `docs/${thread.parent.name.toLowerCase()}`;
-
-    const filePath = `workspace/${dir}/${filename}.md`;
-
-    await fs.mkdir(`workspace/${dir}`, { recursive: true });
-
-    await fs.writeFile(filePath, markdownContent);
-    await registry(thread, dir);
-    return filePath;
-}
-
-async function registry(thread, filePath){
-    const registry = await loadRegistry();
-    registry[thread.id] = filePath.replace("workspace/", "");
-    await saveRegistry(registry, filePath)
-}
-
-async function loadRegistry() {
-    try {
-        return JSON.parse(await fs.readFile("workspace/.registry.json", "utf-8"));
-    } catch {
-        return {};
+function scheduleSync(thread) {
+    const id = thread.id;
+    if (timers.has(id)) {
+        clearTimeout(timers.get(id));
     }
-}
-
-async function saveRegistry(registry, dir) {
-    await fs.writeFile(
-        `workspace/${dir}/.registry.json`,
-        JSON.stringify(registry, null, 2)
-    );
+    timers.set(id, setTimeout(() => {
+        syncThread(thread);
+        timers.delete(id);
+    }, 10000));
 }
 
 client.login(process.env.DISCORD_TOKEN);
